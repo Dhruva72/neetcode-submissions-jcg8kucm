cd5031fecfd32828c5c9f9c0a8be6a17cef9f610
Dhruva Parode <dhruvaparode2@gmail.com>
2026-06-01 10:37:59 +0530

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public void reverseString(char[] s) {
+        char[] tmp = new char[s.length];
+        for (int i = s.length - 1, j = 0; i >= 0; i--, j++) {
+            tmp[j] = s[i];
+            temp;
+        }
+        //for (int i = 0; i < s.length; i++) {
+        //    s[i] = tmp[i];
+        //}
+    
+
+    }
+} 
